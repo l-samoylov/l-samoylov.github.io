6dcd7ec257ae8c7ba5558ca3fea6eed25440096e
l-samoylov <172438414+l-samoylov@users.noreply.github.com>
2025-08-13 22:40:22 +0000

    Deploying to gh-pages from @ l-samoylov/l-samoylov.github.io@37fd68d2f4d69577d883a7a94eb5bcbe6c630df7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "CV",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/LS_CV.pdf";
+            window.location.href = "/assets/pdf/LisaSamoylovCV.pdf";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
