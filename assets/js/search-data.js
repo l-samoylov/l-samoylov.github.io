37d31d60ffdbf89f620e545377105efea446cbe4
l-samoylov <172438414+l-samoylov@users.noreply.github.com>
2026-09-07 18:21:03 +0000

    Deploying to gh-pages from @ l-samoylov/l-samoylov.github.io@d91f1a50eaf91f411e37d6f3ddbbdb3a77d1b529 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,9 +9,16 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "🚀 Incoming! (*indicates that authors are listed alphabetically)",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
           title: "projects",
-          description: "Projects I&#39;ve completed as part of coursework, and in my free time! ⛏️",
+          description: "Projects I&#39;ve completed as part of coursework and in my free time! ⛏️",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
